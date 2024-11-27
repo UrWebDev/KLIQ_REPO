18eb6dab1b096860ddf361170094d57a2356f5e4
Jojo Maninang <110759203+UrWebDev@users.noreply.github.com>
2024-11-28 01:04:40 +0800

    removed files

--- frontendkliq/app/_layout.jsx
+++ frontendkliq/app/_layout.jsx
@@ -5,9 +5,6 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Welcome' }} />
-      <Stack.Screen name="auth" options={{ title: 'Authentication' }} />
-      <Stack.Screen name="recipients" options={{ title: 'Recipient' }} />
-      <Stack.Screen name="user" options={{ title: 'User' }} />
     </Stack>
   );
 }
