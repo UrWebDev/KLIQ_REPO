75934eb4ccd2f82453afc7f6b9c19b1c0bc6a224
Jojo Maninang <110759203+UrWebDev@users.noreply.github.com>
2025-01-20 17:10:12 +0800

    Update proxy.js

--- kliq-backend/api/proxy.js
+++ kliq-backend/api/proxy.js
@@ -1,13 +1,14 @@
 // /backend/api/proxy.js
 
-const httpProxy = require('http-proxy');
+import httpProxy from 'http-proxy';
 
 // Create a proxy server
 const proxy = httpProxy.createProxyServer({});
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const target = 'https://kliq-repo-backend.vercel.app'; // Replace with your target server for proxying
 
   // Forward the request to the target HTTPS server
   proxy.web(req, res, { target });
-};
+  console.log("PROXY WORKING");
+}
