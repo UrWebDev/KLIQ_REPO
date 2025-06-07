31de6c6de7410109305988418f1c8d4be879743d
Jojo Maninang <110759203+UrWebDev@users.noreply.github.com>
2025-06-07 20:40:01 +0800

    On boarding tutorial
    added images for onboarding tutorial

--- frontendkliq/app/_layout.jsx
+++ frontendkliq/app/_layout.jsx
@@ -1,10 +1,36 @@
 // app/_layout.jsx
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
 
 export default function RootLayout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      try {
+        const hasSeen = await AsyncStorage.getItem('hasSeenOnboarding');
+        if (!hasSeen) {
+          router.replace('/onboardTut');
+        }
+      } catch (e) {
+        console.error('Failed to check onboarding:', e);
+      }
+    };
+
+    checkOnboarding();
+  }, []);
+  useEffect(() => {
+  if (__DEV__) {
+    AsyncStorage.removeItem('hasSeenOnboarding');
+  }
+}, []);
+
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name="index" options={{ title: 'Welcome', headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: 'Welcome' }} />
+      <Stack.Screen name="onboardTut" options={{ headerShown: false }} />
     </Stack>
   );
 }
